3a4cdb5ed608e9b8890bcfe1aca604f6819370dd
Jakub Stibůrek <alcolp96@gmail.com>
2022-08-26 14:01:45 +0200

    monster components

--- src/components.rs
+++ src/components.rs
@@ -6,5 +6,22 @@ pub struct Render {
     pub glyph: FontCharType
 }
 
+enum Entities {
+    Player,
+    Monster
+}
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Player;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Goblin;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Orc;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Ogre;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Ettin;
